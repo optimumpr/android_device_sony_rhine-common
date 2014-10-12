75ea098af9327002aece6d3709fdb923518f58c8
david <davidteri91@gmail.com>
2014-10-12 15:18:21 +0200

    Audio Configuration

--- rhine.mk
+++ rhine.mk
@@ -86,6 +86,10 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+
 # Camera
 PRODUCT_PACKAGES += \
     CameraWorkaround
